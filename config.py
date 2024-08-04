5d24cd03495bc17db66d9a81e9526d6772d48851
Bonanzamall <fierte704@example.com>
2024-08-04 17:23:34 +0200

    Ajout du dossier uploads_file et configuration des fichiers téléchargés

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     SECRET_KEY = 'nous516024'
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.jmiotireachftycaknlh:Marti%4012345nous@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'path/to/upload/folder'
+    UPLOAD_FOLDER = 'uploads_file'
